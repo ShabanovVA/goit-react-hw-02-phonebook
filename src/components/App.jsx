b249d405ce0ab985c6b7a9428bfc7f63c470953c
ShabanovVA <1514768zp@gmail.com>
2023-03-30 01:16:10 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+import {Form} from "./Form";
+// import { ContactList } from "./ContactList";
+export class App extends Component {
+state = {
+  contacts: [],
+  // name: '',
+  // number: ''
+  }
+  
+  createContact = data => {
+    console.log(data);
+    this.setState(prevState => {
+      return { contacts: [data, ...prevState.contacts] };
+    });
+  };
+  // handleChange = (e) => {
+  //   const { name, value } = e.currentTarget;
+  //   this.setState({ [name]:value })
+  // };
+
+  // handleSubmit = (e) => {
+  //   e.preventDefault()
+    // this.setState((prevState) => ({ contacts: [...prevState.contacts,this.state.name,this.state.number] }))  
+    // this.setState({ name: '', number:''});
+  // };
+
+  render() {
+    return (
+      <><Form onClickSubmit={this.createContact} />
+      <h2>Contacts</h2>
+      <p>{[this.state.contacts]}</p></>
+    )
+  }
+}
